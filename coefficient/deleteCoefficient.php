17ac6945f44e9ca97d1a7130986ea7df6fa5f810
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-10 22:42:53 +0300

    Update deleteCoefficient.php

--- coefficient/deleteCoefficient.php
+++ coefficient/deleteCoefficient.php
@@ -1,10 +1,13 @@
 <?php
     require_once(dirname(__DIR__) . '/configDB.php');
+
     $sqlCoefficentID = file_get_contents(dirname(__DIR__) . '/sql/deleteCoefficient.sql');
     $sqlCascade = file_get_contents(dirname(__DIR__) . '/sql/deleteCoefficientCascade.sql');
+
     $arguments= [
         'id' => $_GET['id']
     ];
+
     execution($sqlCoefficentID, $arguments);
     execution($sqlCascade, $arguments);
 ?>
