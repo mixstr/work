c1829f05b9e20b0c64e95e2bc2dec4994ce11818
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-11 00:29:21 +0300

    Update deleteMonth.php

--- month/deleteMonth.php
+++ month/deleteMonth.php
@@ -1,10 +1,13 @@
 <?php
-    require_once(dirname(__DIR__) . '/configDB.php');
-    $sqlMonthID = file_get_contents(dirname(__DIR__) . '/sql/deleteMonth.sql');
-    $sqlCascade = file_get_contents(dirname(__DIR__) . '/sql/deleteMonthCascade.sql');
+    require(dirname(__DIR__) . '/configDB.php');
+
+    $deleteMonthSql = file_get_contents(dirname(__DIR__) . '/sql/deleteMonth.sql');
+    $deleteCascadeSql = file_get_contents(dirname(__DIR__) . '/sql/deleteMonthCascade.sql');
+
     $arguments= [
         'id' => $_GET['id']
     ];
-    execution($sqlMonthID, $arguments);
-    execution($sqlCascade, $arguments);
+
+    execution($deleteMonthSql, $arguments);
+    execution($deleteCascadeSql, $arguments);
 ?>
