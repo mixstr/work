634dc3e883364e7f398f951233e65eb1539ff948
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-10 22:43:15 +0300

    Update selectСoefficient.php

--- coefficient/selectСoefficient.php
+++ coefficient/selectСoefficient.php
@@ -1,7 +1,9 @@
 <?php
     require_once(dirname(__DIR__) . '/configDB.php');
+
     $sql = file_get_contents(dirname(__DIR__) . '/sql/selectCoefficient.sql');
     $query = $databasehandler->query($sql);
+
     foreach ($query as $row) {
         print "<br/>";
         print $row['id'] . '-' . $row['employee_id'] . '-' . $row['month_id'] . '-' . $row['coefficient'] . '<br/>';
