3dd93a0686ff96b37b14570d565f7e8a47b2ee2a
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-10 22:17:53 +0300

    Update selectEmployees.php

--- employees/selectEmployees.php
+++ employees/selectEmployees.php
@@ -2,6 +2,7 @@
     require_once(dirname(__DIR__) . '/configDB.php');
     $sql = file_get_contents(dirname(__DIR__) . '/sql/selectEmployees.sql');
     $query = $databasehandler->query($sql);
+
     foreach ($query as $row) {
         print "<br/>";
         print $row['id'] . '-' . $row['fio'] . '-' . $row['hire_date'] . '-' . $row['termination_date'] . '<br/>';
