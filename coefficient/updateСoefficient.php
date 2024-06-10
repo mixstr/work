ff48a4e62519c11e9624d0bea4c7d27ccb03996a
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-11 00:26:08 +0300

    Update updateСoefficient.php

--- coefficient/updateСoefficient.php
+++ coefficient/updateСoefficient.php
@@ -1,7 +1,8 @@
 <?php
-    require_once(dirname(__DIR__) . '/configDB.php');
+    require(dirname(__DIR__) . '/configDB.php');
 
     $sql = file_get_contents(dirname(__DIR__) . '/sql/updateCoefficient.sql');
+
     $arguments = [
         'id' => $_GET['id'],
         'employee_id' => $_GET['employee_id'],
