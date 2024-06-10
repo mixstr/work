01d2e79d75a033b24f7c3356ac3f7be6cc966590
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-11 00:28:34 +0300

    Update updateEmployees.php

--- employees/updateEmployees.php
+++ employees/updateEmployees.php
@@ -1,11 +1,14 @@
 <?php
-    require_once(dirname(__DIR__) . '/configDB.php');
+    require(dirname(__DIR__) . '/configDB.php');
+
     $sql = file_get_contents(dirname(__DIR__) . '/sql/updateEmployees.sql');
+
     $arguments = [
         'id' => $_GET['id'],
         'FIO' => $_GET['FIO'],
         'hire_date' => $_GET['hire_date'],
         'termination_date' => $_GET['termination_date'],
     ];
+
     execution($sql, $arguments);
 ?>
