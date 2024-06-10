f287297b3ad1738aa4a3eadf22ffa5737462e2b1
mixstr <168008095+mixstr@users.noreply.github.com>
2024-06-10 22:16:38 +0300

    Update deleteEmployees.php

--- employees/deleteEmployees.php
+++ employees/deleteEmployees.php
@@ -2,11 +2,9 @@
     require_once(dirname(__DIR__) . '/configDB.php');
     $deleteEmployees = file_get_contents(dirname(__DIR__) . '/sql/deleteEmployees.sql');
     $monthIds = array();
-
     $arguments= [
         'id' => $_GET['id']
     ];
-
     $sqlMonthID = file_get_contents(dirname(__DIR__) . '/sql/cascadeMonthToEmployees.sql');
     $result = execution($sqlMonthID, $arguments);
     $monthIds = $result->fetchAll(PDO::FETCH_COLUMN);
@@ -18,7 +16,6 @@
     }
 
     $deleteCoefficient = file_get_contents(dirname(__DIR__) . '/sql/deleteEmployeesCascadeCoefficient.sql');
-
     execution($deleteCoefficient, $arguments);
     execution($deleteEmployees, $arguments);
 ?>
